75eb7fa8b71777d6498f2a9382a59801918c9ace
DZLZH <dzlzh@null.net>
2016-11-15 15:06:58 +0100

    add function and config

---
+++ config.example.php
@@ -0,0 +1,30 @@
+<?PHP
+/**
+ *  +--------------------------------------------------------------
+ *  | Copyright (c) 2016 DZLZH All rights reserved.
+ *  +--------------------------------------------------------------
+ *  | Author: DZLZH <dzlzh@null.net>
+ *  +--------------------------------------------------------------
+ *  | Filename: config.example.php
+ *  +--------------------------------------------------------------
+ *  | Last modified: 2016-11-15 20:58
+ *  +--------------------------------------------------------------
+ *  | Description: 
+ *  +--------------------------------------------------------------
+ */
+
+require_once 'function.php';
+
+$userAgent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.87 Safari/537.36';
+
+$url = array(
+    'root'    => 'https://www.shiyanlou.com',
+    'login'   => 'https://www.shiyanlou.com/login',
+    'courses' => 'https://www.shiyanlou.com/courses/',
+);
+
+
+$loginParam = array(
+    'login'      => '',
+    'password'   => '',
+);
---
+++ function.php
@@ -0,0 +1,52 @@
+<?PHP
+/**
+ *  +--------------------------------------------------------------
+ *  | Copyright (c) 2016 DZLZH All rights reserved.
+ *  +--------------------------------------------------------------
+ *  | Author: DZLZH <dzlzh@null.net>
+ *  +--------------------------------------------------------------
+ *  | Filename: function.php
+ *  +--------------------------------------------------------------
+ *  | Last modified: 2016-11-15 20:57
+ *  +--------------------------------------------------------------
+ *  | Description: 
+ *  +--------------------------------------------------------------
+ */
+
+
+function curl_html($url, $userAgent = null, $cookie = null, $param = null, $file = null, $header = 0)
+{
+    for ($i = 0; $i < 3; $i++) {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HEADER, $header);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        if($userAgent != null) {
+            curl_setopt($curl, CURLOPT_USERAGENT, $userAgent);
+        }
+        if($param != null) {
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $param);
+        }
+        if($cookie != null) {
+            curl_setopt($curl, CURLOPT_COOKIE, $cookie);
+        }
+        if ($file != null) {
+            $fp = fopen($file, 'w');
+            curl_setopt($curl, CURLOPT_FILE, $fp);
+        }
+        $data = curl_exec($curl);
+        $info = curl_getinfo($curl);
+        curl_close($curl);
+        $httpCodeIsMatched = preg_match('/4\d{2}|5\d{2}/', $info['http_code']);
+        if (!$httpCodeIsMatched) {
+            return $data;
+        }
+        usleep(1000000 * $i);
+    }
+    $error = 'Error::http_code:' . $info['http_code'] . ';url:' . $info['url'];
+    if (is_array($param)) {
+        $error .= ';param:' . '(' . serialize($param) . ')';
+    }
+    return $error;
+}
