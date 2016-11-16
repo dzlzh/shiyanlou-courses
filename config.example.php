d7b3f6a269e05e8ef16fca8b6d4591922bacdae4
DZLZH <dzlzh@null.net>
2016-11-16 14:18:00 +0100

    courses url

--- config.example.php
+++ config.example.php
@@ -20,11 +20,59 @@ $userAgent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML
 $url = array(
     'root'    => 'https://www.shiyanlou.com',
     'login'   => 'https://www.shiyanlou.com/login',
-    'courses' => 'https://www.shiyanlou.com/courses/',
+    'courses' => 'https://www.shiyanlou.com/courses/?course_type=all',
 );
 
 
 $loginParam = array(
     'login'      => '',
     'password'   => '',
+);
+
+$coursesParam = array(
+    //标签
+    'tag'  => 'php',
+
+    //类别
+    //free    免费
+    //limited 限免
+    //member  会员
+    'fee' => 'free',
+
+    //排序
+    //latest  最新
+    //hotest  最热
+    'order' => 'latest',
+
+    //页码
+    // 'page'  => '1',
+);
+
+$tag = array(
+    'Python',
+    'C/C++',
+    'Linux',
+    'Web',
+    'PHP',
+    'Java',
+    '信息安全',
+    'NodeJS',
+    'Android',
+    'GO',
+    '计算机专业课',
+    'Spark',
+    'Hadoop',
+    'HTML5',
+    'Scala',
+    'SQL',
+    'Ruby',
+    'R',
+    '网络',
+    'Git',
+    'NoSQL',
+    '算法',
+    'Docker',
+    'Swift',
+    '汇编',
+    'Windows',
 );
--- courses.php
+++ courses.php
@@ -15,23 +15,50 @@
 
 require_once 'config.php';
 
-$login = curl_html($url['login'], $userAgent, null, null, null, 1);
-$cookieIsMatched = preg_match('/Set-Cookie:\s*(.*)/', $login, $matches);
-if ($cookieIsMatched) {
-    $cookie = strstr($matches[1], ';', true);
+if (!file_exists('cookie')) {
+    var_dump(1);
+    $login = curl_html($url['login'], $userAgent, null, null, null, 1);
+    $cookieIsMatched = preg_match('/Set-Cookie:\s*(.*)/', $login, $matches);
+    if ($cookieIsMatched) {
+        $cookie = strstr($matches[1], ';', true);
+    }
+
+    $csrfTokenIsMatched = preg_match('/csrf_token.*value="([^"]*)"/', $login, $matches);
+    if ($csrfTokenIsMatched) {
+        $loginParam['csrf_token'] = $matches[1];
+    }
+
+    $login = curl_html($url['login'], $userAgent, $cookie, $loginParam, null, 1);
+    $cookieIsMatched = preg_match('/Set-Cookie:\s*(.*)/', $login, $matches);
+    if ($cookieIsMatched) {
+        $cookie .= ';' . strstr($matches[1], ';', true);
+    }
+    file_put_contents('cookie', $cookie) || exit;
 }
+$cookie = file_get_contents('cookie');
+if ($cookie) {
+    foreach ($tag as $key => $value) {
+        $coursesParam['tag'] = $value;
+        $coursesTagUrl = $url['courses'];
+        foreach ($coursesParam as $k => $v) {
+            $coursesTagUrl .= '&' . $k . '=' . urlencode($v);
+        }
+        $page = 1;
+        $coursesUrl = array();
+        while(true) {
+            $tagUrl = $coursesTagUrl . '&page=' . $page;
+            echo $tagUrl , "\n";
+            $tagHtml = curl_html($tagUrl, $userAgent, $cookie);
+            $isMatched = preg_match_all('/<a[^(class)]*class="course-box"[^(href)]*href="([^"]+)">/i', $tagHtml, $matches);
+            if ($isMatched) {
+                $coursesUrl = array_merge_recursive($coursesUrl, $matches[1]);
+            } else {
+                break;
+            }
+            $page++;
+        }
+        print_r($coursesUrl);
+        die;
+    }
 
-$csrfTokenIsMatched = preg_match('/csrf_token.*value="([^"]*)"/', $login, $matches);
-if ($csrfTokenIsMatched) {
-    $loginParam['csrf_token'] = $matches[1];
 }
-
-$login = curl_html($url['login'], $userAgent, $cookie, $loginParam, null, 1);
-$cookieIsMatched = preg_match('/Set-Cookie:\s*(.*)/', $login, $matches);
-if ($cookieIsMatched) {
-    $cookie .= ';' . strstr($matches[1], ';', true);
-}
-
-
-var_dump($cookie);
-var_dump(curl_html('https://www.shiyanlou.com/courses/63/labs/291/document', $userAgent, $cookie));
