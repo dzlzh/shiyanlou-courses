72532b6c074c1598b12d05f576bc0f4639f5fa60
DZLZH <dzlzh@null.net>
2016-11-16 16:01:18 +0100

    course name and url

--- courses.php
+++ courses.php
@@ -35,19 +35,25 @@ if (!file_exists('cookie')) {
     }
     file_put_contents('cookie', $cookie) || exit;
 }
+
 $cookie = file_get_contents('cookie');
 if ($cookie) {
+    //循环类型
     foreach ($tag as $key => $value) {
+        // echo $value, "\n";
+        //目录
         $coursesParam['tag'] = $value;
         $coursesTagUrl = $url['courses'];
         foreach ($coursesParam as $k => $v) {
+            //当前类型URL
             $coursesTagUrl .= '&' . $k . '=' . urlencode($v);
         }
         $page = 1;
         $coursesUrl = array();
+        //当前类型全部课程URL
         while(true) {
             $tagUrl = $coursesTagUrl . '&page=' . $page;
-            echo $tagUrl , "\n";
+            // echo $tagUrl , "\n";
             $tagHtml = curl_html($tagUrl, $userAgent, $cookie);
             $isMatched = preg_match_all('/<a[^(class)]*class="course-box"[^(href)]*href="([^"]+)">/i', $tagHtml, $matches);
             if ($isMatched) {
@@ -57,7 +63,27 @@ if ($cookie) {
             }
             $page++;
         }
-        print_r($coursesUrl);
+
+        foreach ($coursesUrl as $courseUrl) {
+            // echo $courseUrl, "\n";
+            $courseHtml = curl_html($url['root'] . $courseUrl, $userAgent, $cookie);
+            //课程名字
+            $isMatched = preg_match('/<h4\s*[^(class)]*class="pull-left[^>]*>\s*<span>([^<]+)<\/span>\s*<\/h4>/i', $courseHtml, $matches);
+            if ($isMatched) {
+                $courseName = $matches[1];
+                $contents[$value][] = $courseName;
+            }
+            //课程内容URL
+            $isMatched = preg_match_all('/\/courses\/\d+\/labs\/\d+\/document/i', $courseHtml, $matches);
+            if ($isMatched) {
+                //课程内容
+                foreach ($matches[0] as $documentUrl) {
+                    // echo $documentUrl, "\n";
+                    die;
+                }
+            }
+            die;
+        }
         die;
     }
 
